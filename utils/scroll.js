1a98ff408bac655a795a7bd90c5158b2d33e7885
Diego Arndt <diegoarndt@outlook.com>
2023-02-26 01:16:08 -0500

    Create scroll.js

---
+++ utils/scroll.js
@@ -0,0 +1,39 @@
+import { Link } from 'react-scroll';
+import { useRouter } from 'next/router';
+import { useState, useRef } from 'react';
+
+export default function ScrollLink({ to, children }) {
+  const router = useRouter();
+  const [isActive, setIsActive] = useState(false);
+  const targetRef = useRef(null);
+  const isLanding = to === 'landing';
+
+  const handleClick = () => {
+    setIsActive(true);
+
+    router.push(`${isLanding ? '' : '/#' + to}`, undefined, {
+      scroll: false,
+      shallow: true,
+    });
+  };
+
+  return (
+    <Link
+      to={to}
+      offset={isLanding ? -500 : 0}
+      smooth={true}
+      duration={1000}
+      spy={true}
+      exact='true'
+      activeClass='active'
+      onSetActive={() => setIsActive(true)}
+      onSetInactive={() => setIsActive(false)}
+      onClick={handleClick}
+      className='relative sm:h-16 sm:w-16 cursor-pointer'
+    >
+      <span className={isActive ? 'active' : ''} ref={targetRef}>
+        {children}
+      </span>
+    </Link>
+  );
+}
