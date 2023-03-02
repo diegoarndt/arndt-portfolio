d587debde3e8cdaf481a289c590491a2f936db5e
Diego Arndt <diegoarndt@outlook.com>
2023-03-01 22:47:35 -0500

    Default language from user's browser

--- pages/_app.js
+++ pages/_app.js
@@ -3,33 +3,33 @@ import { Analytics } from '@vercel/analytics/react';
 
 import en from '../locales/en.json';
 import es from '../locales/es.json';
-import br from '../locales/br.json';
+import pt from '../locales/pt.json';
 import de from '../locales/de.json';
 
 import '../styles/globals.css';
 
 const translation = {
   en,
-  br,
+  pt,
   es,
   de,
 };
 
 const countryCodes = {
   en: 'CA',
-  br: 'BR',
+  pt: 'BR',
   es: 'ES',
   de: 'DE',
 };
 
 const customLabels = {
   CA: 'English',
-  BR: 'Português',
+  PT: 'Português',
   ES: 'Español',
   DE: 'Deutsch',
 };
 
-function ArndtPortfolio({ Component, pageProps }) {
+function ArndtPortfolio({ Component, pageProps, initialLanguage }) {
   const { locale, asPath, push } = useRouter();
   pageProps = {
     ...pageProps,
@@ -43,10 +43,22 @@ function ArndtPortfolio({ Component, pageProps }) {
 
   return (
     <>
-      <Component {...pageProps} />;
+      <Component {...pageProps} initialLanguage={initialLanguage} />;
       <Analytics />
     </>
   );
+}
+
+export async function getServerSideProps(context) {
+  const { req } = context;
+  const acceptLanguage = req.headers['accept-language'] || '';
+  const preferredLanguage = acceptLanguage.split(',')[0].trim().split('-')[0] || 'en';
+
+  return {
+    props: {
+      initialLanguage: preferredLanguage,
+    },
+  };
 }
 
 export default ArndtPortfolio;
