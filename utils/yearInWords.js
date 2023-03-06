728edc8ca6f059158ace9859194868e78d1df0b0
Diego Arndt <diegoarndt@outlook.com>
2023-03-05 19:24:43 -0500

    Update yearInWords.js
    When the language is not english, the year will be displayed in a number format, e.g.: 2023

--- utils/yearInWords.js
+++ utils/yearInWords.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-function YearInWords(language) {
+function YearInWords({ language }) {
   const year = new Date().getFullYear();
+
+  if (language !== 'en') {
+    return <span>{year}</span>;
+  }
+
   const englishWordMap = {
     0: '',
     1: 'one',
@@ -33,101 +38,6 @@ function YearInWords(language) {
     90: 'ninety',
   };
 
-  const deutschWordMap = {
-    0: '',
-    1: 'eins',
-    2: 'zwei',
-    3: 'drei',
-    4: 'vier',
-    5: 'fünf',
-    6: 'sechs',
-    7: 'sieben',
-    8: 'acht',
-    9: 'neun',
-    10: 'zehn',
-    11: 'elf',
-    12: 'zwölf',
-    13: 'dreizehn',
-    14: 'vierzehn',
-    15: 'fünfzehn',
-    16: 'sechzehn',
-    17: 'siebzehn',
-    18: 'achtzehn',
-    19: 'neunzehn',
-    20: 'zwanzig',
-    30: 'dreißig',
-    40: 'vierzig',
-    50: 'fünfzig',
-    60: 'sechzig',
-    70: 'siebzig',
-    80: 'achtzig',
-    90: 'neunzig',
-  };
-  
-  const espanolWordMap = {
-    0: '',
-    1: 'uno',
-    2: 'dos',
-    3: 'tres',
-    4: 'cuatro',
-    5: 'cinco',
-    6: 'seis',
-    7: 'siete',
-    8: 'ocho',
-    9: 'nueve',
-    10: 'diez',
-    11: 'once',
-    12: 'doce',
-    13: 'trece',
-    14: 'catorce',
-    15: 'quince',
-    16: 'dieciséis',
-    17: 'diecisiete',
-    18: 'dieciocho',
-    19: 'diecinueve',
-    20: 'veinte',
-    30: 'treinta',
-    40: 'cuarenta',
-    50: 'cincuenta',
-    60: 'sesenta',
-    70: 'setenta',
-    80: 'ochenta',
-    90: 'noventa',
-  };
-
-  const portuguesWordMap = {
-    0: '',
-    1: 'um',
-    2: 'dois',
-    3: 'três',
-    4: 'quatro',
-    5: 'cinco',
-    6: 'seis',
-    7: 'sete',
-    8: 'oito',
-    9: 'nove',
-    10: 'dez',
-    11: 'onze',
-    12: 'doze',
-    13: 'treze',
-    14: 'catorze',
-    15: 'quinze',
-    16: 'dezesseis',
-    17: 'dezessete',
-    18: 'dezoito',
-    19: 'dezenove',
-    20: 'vinte',
-    30: 'trinta',
-    40: 'quarenta',
-    50: 'cinquenta',
-    60: 'sessenta',
-    70: 'setenta',
-    80: 'oitenta',
-    90: 'noventa',
-  };
-
-  // TO DO: return based on language
-
   const getWord = (number) => {
     if (number === 0) {
       return '';
@@ -140,7 +50,8 @@ function YearInWords(language) {
     return englishWordMap[tens] + (ones ? '-' + englishWordMap[ones] : '');
   };
 
-  const yearInWords = getWord(Math.floor(year / 100)) + ' ' + getWord(year % 100);
+  const yearInWords =
+    getWord(Math.floor(year / 100)) + ' ' + getWord(year % 100);
   return <span>{yearInWords}</span>;
 }
 
